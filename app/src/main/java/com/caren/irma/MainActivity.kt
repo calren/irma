ab7845545f75bf3119021645d92a9a0251f1bda2
Caren Chang <carenc@google.com>
2020-09-27 13:47:37 -0700

    First step to set up Cloud Firestore. Add simple call for getting books.

--- app/src/main/java/com/caren/irma/MainActivity.kt
+++ app/src/main/java/com/caren/irma/MainActivity.kt
@@ -2,10 +2,25 @@ package com.caren.irma
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getBooks()
+    }
+
+    fun getBooks() {
+        val db = FirebaseFirestore.getInstance()
+
+        db.collection("books").get().addOnSuccessListener {
+            val books = it.documents
+            for (book in books) {
+                Log.i("Caren", "book " + book.get("title") + " " + book.get("date_added"))
+            }
+        }
     }
 }
